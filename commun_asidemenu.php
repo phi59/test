0a3482e1f211c2eaced728973c2e373889b7e838
phi59 <ph.couche.pro@gmail.com>
2017-04-28 17:27:13 +0200

    création de toutes les pages du menu (en mode travaux)

--- commun_asidemenu.php
+++ commun_asidemenu.php
@@ -1,11 +1,11 @@
 ﻿			<aside>
 				<nav>
 					<ul>
-						<li><a href="index.php" title="Retour à l'accueil">accueil</a></li>
-						<li><a href="index.php" title="Répertoire actuel">répertoire</a></li>
-						<li><a href="index.php" title="Prochains événements">agenda</a></li>
-						<li><a href="index.php" title="Nous contacter">contact</a></li>
-						<li><a href="index.php" title="Pour les choristes uniquement">accès choriste</a></li>
+						<li><a href="index.php"         title="Retour à l'accueil">accueil</a></li>
+						<li><a href="repertoire.php"    title="Répertoire actuel">répertoire</a></li>
+						<li><a href="prochainement.php" title="Prochains événements">agenda</a></li>
+						<li><a href="contact.php"       title="Nous contacter">contact</a></li>
+						<li><a href="choristes.php"     title="Pour les choristes uniquement">accès choriste</a></li>
 					</ul>
 				</nav>
 			</aside>
